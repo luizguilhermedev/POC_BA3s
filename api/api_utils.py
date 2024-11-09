7d05fef4f305610f8adea4e334a831cb600ec611
luizguilhermedev <luizg_dev@outlook.com>
2024-11-09 11:30:00 -0300

    - fixing routes and codebase structure - adding entrypoint to be used by docker

--- api/api_utils.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-"""This module is intended to be used as a utility for the FatsAPI API. It contains te pydantic  models to be used as a
-message payload for the API endpoints."""
-
-
-class MessagePayload(BaseModel):
-    input: str
