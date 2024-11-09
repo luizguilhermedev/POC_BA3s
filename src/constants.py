34ae79c079a2a4562d97eb8248292d15bce19370
luizguilhermedev <luizg_dev@outlook.com>
2024-11-09 10:20:28 -0300

    adding chat route creating csv chat agent

--- src/constants.py
+++ src/constants.py
@@ -2,5 +2,3 @@ CSV_FILES = [
     'data/books_data_cleaned.csv',
     'data/books_rating_cleaned.csv',
 ]
-
-
--- src/utils.py
+++ src/utils.py
@@ -10,8 +10,7 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 def get_llm_model():
     model_openai = ChatOpenAI(
-        temperature=0.2,
-        model="gpt-4o",
-        openai_api_key=OPENAI_API_KEY)
+        temperature=0.2, model='gpt-4o', openai_api_key=OPENAI_API_KEY
+    )
 
     return model_openai
